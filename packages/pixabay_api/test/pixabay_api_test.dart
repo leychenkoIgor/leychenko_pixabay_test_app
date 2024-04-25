ca8cdeebc0bdbd587dd98f91e2534a0a078873e5
thorleichenko <leychenko.igor@gmail.com>
2024-04-25 17:55:25 +0300

    add unit test to package api

--- packages/pixabay_api/test/pixabay_api_test.dart
+++ packages/pixabay_api/test/pixabay_api_test.dart
@@ -1,11 +1,19 @@
-// import 'package:flutter_test/flutter_test.dart';
 import 'package:pixabay_api/pixabay_api.dart';
+import 'package:test/test.dart';
 
-//
-void main() async {
+void main() {
   PixabayAPI pixabayAPI =
       PixabayAPI("https://pixabay.com", "43471674-3c93ea4a2a220b169f739aaee");
-
-  // ignore: unused_local_variable
-  Photos photos = await pixabayAPI.getPhotos();
+  Photos photos;
+  test('Test connect to pixabay.com', () async {
+    try {
+      photos = await pixabayAPI.getPhotos();
+    } on Exception catch (e) {
+      print(e);
+      throw "";
+    }
+    if (photos.hits.isEmpty) {
+      throw "ERROR:  An empty array of photos was received from the server.";
+    }
+  });
 }
