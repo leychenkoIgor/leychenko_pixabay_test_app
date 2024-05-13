8d7a6315c9d42ed84e8308f7e87c1e44098e58a4
thorleichenko <leychenko.igor@gmail.com>
2024-05-13 15:09:13 +0300

    edit config

--- lib/services/envelopments/environment_config.dart
+++ lib/services/envelopments/environment_config.dart
@@ -1,7 +1,7 @@
 class EnvironmentConfig {
-  static const String apiUrl = 'https://pixabay.com';
+  static const String apiUrl = 'API_URL';
 
-  static const String apiKey = '43471674-3c93ea4a2a220b169f739aaee';
+  static const String apiKey = 'APY_KEY';
 
   static const List<String> supportedLocales = ["uk", "en", "pl", "fr"];
 }
